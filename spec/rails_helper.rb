57da422be3e7ed37e928ed43362c0c37f434468d
Davis Weimer <davisweimer@gmail.com>
2024-11-01 20:03:06 -0600

    Add Devise test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Add Devise test identifiers sign_in & sign_out
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
