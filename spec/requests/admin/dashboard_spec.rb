86a0da0f9cfbdeecd97f24e27bff179405dfa35d
Davis Weimer <davisweimer@gmail.com>
2024-11-01 21:05:14 -0600

    Add test for endpoint

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Dashboards", type: :request do
+  let(:user) { User.create(email: "test@test.com", password: "password" ) }
+
+  before do
+    sign_in user
+  end
   describe "GET /index" do
     it "returns http success" do
-      get "/admin/dashboard/index"
+      get "/admin/dashboard"
       expect(response).to have_http_status(:success)
     end
   end
