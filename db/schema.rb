770f3ecdd6aa4733b0cd75f2b97e837a8d753f3e
Davis Weimer <davisweimer@gmail.com>
2024-11-03 14:09:45 -0700

    Adjust status data type to int

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_03_202542) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_03_204358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,11 +29,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_03_202542) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "status", default: "Draft"
     t.string "slug"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["title"], name: "index_posts_on_title", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
