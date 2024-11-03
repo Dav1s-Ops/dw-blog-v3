e4670ae3ebc6e4e0767afcaade64e633c1d316c8
Davis Weimer <davisweimer@gmail.com>
2024-11-03 14:21:26 -0700

    Add stats to AdminDashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @total_posts = Post.count
+    @total_categories = Category.count
+    @total_tags = Tag.count
+    @draft_posts = Post.where(status: :draft).count
+    @posts_by_category = Category.joins(:posts).group("categories.name").count
   end
 end
