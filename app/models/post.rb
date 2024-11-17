dd1e95cd91cd7fea8041d5cdc4581a6400c5245b
Davis Weimer <davisweimer@gmail.com>
2024-11-17 15:47:49 -0700

    Add getter/setter for Post comma-separated Tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,26 @@ class Post < ApplicationRecord
 
   validates :content, presence: true
   validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validate :must_have_at_least_one_category
+  validate :must_have_at_least_one_tag
+
+  def tag_list
+    @tag_list || tags.pluck(:name).join(", ")
+  end
+
+  def tag_list=(names)
+    @tag_list = names
+  end
+
+  private
+
+  def must_have_at_least_one_category
+    errors.add(:categories, "must have at least one category") if categories.empty?
+  end
+
+  def must_have_at_least_one_tag
+    if @tag_list.blank? && tags.empty?
+      errors.add(:tags, "must have at least one tag")
+    end
+  end
 end
