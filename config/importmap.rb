7345374ebc527b71985f917687d94ef775ff50d4
Davis Weimer <davisweimer@gmail.com>
2024-11-21 12:10:53 -0700

    Import and utilize stimulus-components Dropdown

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,12 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+
+pin "stimulus-use" # @0.52.2
+
+pin_all_from "app/javascript/controllers", under: "controllers"
