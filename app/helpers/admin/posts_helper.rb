70937fb1949714aaafa744fbee53e258c16171fa
Davis Weimer <davisweimer@gmail.com>
2024-11-23 13:29:45 -0700

    Add interactive styling to actions column

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,2 +1,8 @@
 module Admin::PostsHelper
+  def admin_action_button(path, icon, label, options = {})
+    link_to path, options.merge(class: "#{options[:class]} admin-posts__action-link-#{label}") do
+      inline_svg_tag(icon, class: "admin-posts__icon") +
+      content_tag(:span, label, class: "admin-posts__action-text")
+    end
+  end
 end
