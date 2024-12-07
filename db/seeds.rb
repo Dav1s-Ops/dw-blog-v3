51fe098bad4ab1a104c47c73dd38c91f262ee9d3
Davis Weimer <davisweimer@gmail.com>
2024-12-06 17:02:53 -0700

    Update seed data!

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ end
 puts "—————— Categories created: #{categories.map(&:name).join(', ')}"
 puts "Creating tags..."
 
-tags = [ 'Beginner', 'Advanced', 'Tips', 'Best Practices', 'Performance' ].map do |tag_name|
+tags = [ 'JavaScript', 'Python', 'Ruby', 'Java', 'C++', 'C#', 'HTML', 'CSS', 'SQL', 'React', 'Angular', 'Vue', 'Node.js', 'Django', 'Flask', 'Rails', 'Spring Boot', 'Git', 'Docker', 'Kubernetes', 'AWS', 'Azure', 'GCP', 'REST', 'GraphQL', 'TypeScript', 'Swift', 'Objective-C', 'Kotlin', 'Go', 'Rust', 'PHP', 'Bash', 'Perl', 'Linux', 'Windows', 'macOS', 'Agile', 'Scrum', 'TDD', 'DevOps', 'Machine Learning', 'AI', 'Data Science', 'Cybersecurity', 'Blockchain', 'Web Development', 'Mobile Development', 'Frontend', 'Backend', 'Fullstack' ].map do |tag_name|
   Tag.create!(name: tag_name)
 end
 
@@ -32,63 +32,14 @@ puts "—————— Tags created: #{tags.map(&:name).join(', ')}"
 puts "Creating posts..."
 
 post_content = <<~MD
-# Headers
-
-```
-# h1 Heading 8-)
+# h1 Heading
 ## h2 Heading
 ### h3 Heading
 #### h4 Heading
 ##### h5 Heading
 ###### h6 Heading
 
-Alternatively, for H1 and H2, an underline-ish style:
-
-Alt-H1
-======
-
-Alt-H2
-------
-```#{'	'}
-
-# h1 Heading 8-)
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-
-Alternatively, for H1 and H2, an underline-ish style:
-
-Alt-H1
-======
-
-Alt-H2
-------
-
-------
-
-# Emphasis
-
-```
-Emphasis, aka italics, with *asterisks* or _underscores_.
-
-Strong emphasis, aka bold, with **asterisks** or __underscores__.
-
-Combined emphasis with **asterisks and _underscores_**.
-
-Strikethrough uses two tildes. ~~Scratch this.~~
-
-**This is bold text**
-
-__This is bold text__
-
-*This is italic text*
-
-_This is italic text_
-
-~~Strikethrough~~
-```
+# Emphasis!
 
 Emphasis, aka italics, with *asterisks* or _underscores_.
 
@@ -108,24 +59,27 @@ _This is italic text_
 
 ~~Strikethrough~~
 
-------
+Programming is the process of designing and building software to solve problems, automate tasks, and create interactive experiences that drive the modern world. It involves writing instructions, called code, that computers interpret to perform specific functions. 
+
+Programming languages like Python, JavaScript, Ruby, and Java provide developers with the tools to express logic, manipulate data, and interact with hardware. Through frameworks and libraries, programmers accelerate development, enabling the creation of everything from responsive web applications to powerful machine learning algorithms. 
+
+It’s a field that blends creativity with logic, requiring attention to detail, problem-solving skills, and an understanding of complex systems. Whether it's crafting a simple script to automate repetitive tasks, building an immersive video game, or engineering scalable cloud services, programming empowers individuals to innovate and bring ideas to life. 
+
+Collaboration is key, as programmers often work in teams, leveraging tools like Git for version control and methodologies like Agile to ensure projects are delivered efficiently. 
+
+As technology evolves, so do programming paradigms, pushing the boundaries of what's possible in artificial intelligence, cybersecurity, and digital transformation. Ultimately, programming isn't just about writing code—it's about building solutions that shape the future.
+
+# Tables
+
+| Header 1      | Header 2      | Header 3      |
+|---------------|---------------|---------------|
+| Row 1 Column 1| Row 1 Column 2| Row 1 Column 3|
+| Row 2 Column 1| Row 2 Column 2| Row 2 Column 3|
+| Row 3 Column 1| Row 3 Column 2| Row 3 Column 3|
+
 
 # Lists
 
-```
-1. First ordered list item
-2. Another item
-⋅⋅* Unordered sub-list.
-1. Actual numbers don't matter, just that it's a number
-⋅⋅1. Ordered sub-list
-4. And another item.
-
-⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
-
-⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
-⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅
-⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
-
 * Unordered list can use asterisks
 - Or minuses
 + Or pluses
@@ -147,192 +101,18 @@ _This is italic text_
     + Facilisis in pretium nisl aliquet
     - Nulla volutpat aliquam velit
 + Very easy!
-```
-
-1. First ordered list item
-2. Another item
-⋅⋅* Unordered sub-list.
-1. Actual numbers don't matter, just that it's a number
-⋅⋅1. Ordered sub-list
-4. And another item.
-
-⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
-
-⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
-⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅
-⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
-
-* Unordered list can use asterisks
-- Or minuses
-+ Or pluses
-
-1. Make my changes
-    1. Fix bug
-    2. Improve formatting
-        - Make the headings bigger
-2. Push my commits to GitHub
-3. Open a pull request
-    * Describe my changes
-    * Mention all the members of my team
-        * Ask for feedback
-
-+ Create a list by starting a line with `+`, `-`, or `*`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet
-    - Nulla volutpat aliquam velit
-+ Very easy!
-
-------
-
-# Task lists
-
-```
-- [x] Finish my changes
-- [ ] Push my commits to GitHub
-- [ ] Open a pull request
-- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
-- [x] list syntax required (any unordered or ordered list supported)
-- [x] this is a complete item
-- [ ] this is an incomplete item
-```
-
-- [x] Finish my changes
-- [ ] Push my commits to GitHub
-- [ ] Open a pull request
-- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
-- [x] list syntax required (any unordered or ordered list supported)
-- [ ] this is a complete item
-- [ ] this is an incomplete item
-
-------
-
-# Ignoring Markdown formatting
-
-You can tell GitHub to ignore (or escape) Markdown formatting by using \ before the Markdown character.
-
-```
-Let's rename \*our-new-project\* to \*our-old-project\*.
-```
-
-Let's rename \*our-new-project\* to \*our-old-project\*.
-
-------
 
 # Links
 
-```
 [I'm an inline-style link](https://www.google.com)
 
 [I'm an inline-style link with title](https://www.google.com "Google's Homepage")
-
-[I'm a reference-style link][Arbitrary case-insensitive reference text]
-
-[I'm a relative reference to a repository file](../blob/master/LICENSE)
 
 [You can use numbers for reference-style link definitions][1]
 
 Or leave it empty and use the [link text itself].
 
-URLs and URLs in angle brackets will automatically get turned into links.
-http://www.example.com or <http://www.example.com> and sometimes
-example.com (but not on Github, for example).
-
-Some text to show that the reference links can follow later.
-
-[arbitrary case-insensitive reference text]: https://www.mozilla.org
-[1]: http://slashdot.org
-[link text itself]: http://www.reddit.com
-```
-
-[I'm an inline-style link](https://www.google.com)
-
-[I'm an inline-style link with title](https://www.google.com "Google's Homepage")
-
-[I'm a reference-style link][Arbitrary case-insensitive reference text]
-
-[I'm a relative reference to a repository file](../blob/master/LICENSE)
-
-[You can use numbers for reference-style link definitions][1]
-
-Or leave it empty and use the [link text itself].
-
-URLs and URLs in angle brackets will automatically get turned into links.
-http://www.example.com or <http://www.example.com> and sometimes
-example.com (but not on Github, for example).
-
-Some text to show that the reference links can follow later.
-
-[arbitrary case-insensitive reference text]: https://www.mozilla.org
-[1]: http://slashdot.org
-[link text itself]: http://www.reddit.com
-
-------
-
-# Images
-
-```
-Here's some images (hover to see the title text):
-
-Inline-style:
-![alt text](https://unsplash.com/photos/an-image-of-a-black-hole-with-a-blue-ring-uHAga4TsXNU "Logo Title Text 1")
-
-Reference-style:
-![alt text][logo]
-
-[logo]: https://unsplash.com/photos/an-image-of-a-black-hole-with-a-blue-ring-uHAga4TsXNU "Logo Title Text 2"
-
-Like links, Images also have a footnote style syntax
-
-![Alt text][id]
-
-With a reference later in the document defining the URL location:
-
-[id]: https://unsplash.com/photos/an-image-of-a-black-hole-with-a-blue-ring-uHAga4TsXNU  "Supermassive black hole. 3D render."
-```
-
-------
-
-# [Footnotes](https://github.com/markdown-it/markdown-it-footnote)
-
-```
-Footnote 1 link[^first].
-
-Footnote 2 link[^second].
-
-Inline footnote^[Text of inline footnote] definition.
-
-Duplicated footnote reference[^second].
-
-[^first]: Footnote **can have markup**
-
-    and multiple paragraphs.
-
-[^second]: Footnote text.
-```
-
-Footnote 1 link[^first].
-
-Footnote 2 link[^second].
-
-Inline footnote^[Text of inline footnote] definition.
-
-Duplicated footnote reference[^second].
-
-[^first]: Footnote **can have markup**
-
-    and multiple paragraphs.
-
-[^second]: Footnote text.
-
-------
-
-# Code and Syntax Highlighting
-
-```
-Inline `code` has `back-ticks around` it.
-```
+# Code Block / Syntax Highlighting
 
 Inline `code` has `back-ticks around` it.
 
@@ -445,102 +225,8 @@ datahere
 datahere */
 datahere
 ```
-
-------
-
-# Tables
-
-```
-Colons can be used to align columns.
-
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-
-There must be at least 3 dashes separating each header cell.
-The outer pipes (|) are optional, and you don't need to make the
-raw Markdown line up prettily. You can also use inline Markdown.
-
-Markdown | Less | Pretty
---- | --- | ---
-*Still* | `renders` | **nicely**
-1 | 2 | 3
-
-| First Header  | Second Header |
-| ------------- | ------------- |
-| Content Cell  | Content Cell  |
-| Content Cell  | Content Cell  |
-
-| Command | Description |
-| --- | --- |
-| git status | List all new or modified files |
-| git diff | Show file differences that haven't been staged |
-
-| Command | Description |
-| --- | --- |
-| `git status` | List all *new or modified* files |
-| `git diff` | Show file differences that **haven't been** staged |
-
-| Left-aligned | Center-aligned | Right-aligned |
-| :---         |     :---:      |          ---: |
-| git status   | git status     | git status    |
-| git diff     | git diff       | git diff      |
-
-| Name     | Character |
-| ---      | ---       |
-| Backtick | `         |
-| Pipe     | \|        |
-```
-
-Colons can be used to align columns.
-
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-
-There must be at least 3 dashes separating each header cell.
-The outer pipes (|) are optional, and you don't need to make the
-raw Markdown line up prettily. You can also use inline Markdown.
-
-Markdown | Less | Pretty
---- | --- | ---
-*Still* | `renders` | **nicely**
-1 | 2 | 3
-
-| First Header  | Second Header |
-| ------------- | ------------- |
-| Content Cell  | Content Cell  |
-| Content Cell  | Content Cell  |
-
-| Command | Description |
-| --- | --- |
-| git status | List all new or modified files |
-| git diff | Show file differences that haven't been staged |
-
-| Command | Description |
-| --- | --- |
-| `git status` | List all *new or modified* files |
-| `git diff` | Show file differences that **haven't been** staged |
-
-| Left-aligned | Center-aligned | Right-aligned |
-| :---         |     :---:      |          ---: |
-| git status   | git status     | git status    |
-| git diff     | git diff       | git diff      |
-
-| Name     | Character |
-| ---      | ---       |
-| Backtick | `         |
-| Pipe     | \|        |
-
-------
-
 # Blockquotes
 
-```
 > Blockquotes are very handy in email to emulate reply text.
 > This line is part of the same quote.
 
@@ -551,61 +237,9 @@ Quote break.
 > Blockquotes can also be nested...
 >> ...by using additional greater-than signs right next to each other...
 > > > ...or with spaces between arrows.
-```
-
-> Blockquotes are very handy in email to emulate reply text.
-> This line is part of the same quote.
-
-Quote break.
-
-> This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.
-
-> Blockquotes can also be nested...
->> ...by using additional greater-than signs right next to each other...
-> > > ...or with spaces between arrows.
-
-------
-
-# Inline HTML
-
-```
-<dl>
-  <dt>Definition list</dt>
-  <dd>Is something people use sometimes.</dd>
-
-  <dt>Markdown in HTML</dt>
-  <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
-</dl>
-```
-
-<dl>
-  <dt>Definition list</dt>
-  <dd>Is something people use sometimes.</dd>
-
-  <dt>Markdown in HTML</dt>
-  <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
-</dl>
-
-------
 
 # Horizontal Rules
 
-```
-Three or more...
-
----
-
-Hyphens
-
-***
-
-Asterisks
-
-___
-
-Underscores
-```
-
 Three or more...
 
 ---
@@ -621,34 +255,16 @@ ___
 Underscores
 
 ------
-
-# YouTube Videos
-
-```
-<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_VIDEO_ID_HERE" target="_blank">
-<img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg" alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10">
-</a>
-```
-
-<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_VIDEO_ID_HERE" target="_blank">
-<img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg" alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10">
-</a>
-
-```
-[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](http://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID_HERE)
-```
-
-[![IMAGE ALT TEXT HERE](https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/YouTube_logo_2015.svg/1200px-YouTube_logo_2015.svg.png)](https://www.youtube.com/watch?v=ciawICBvQoE)
 MD
 
 50.times do |i|
   post = Post.create!(
     title: "Sample Post #{i + 1}",
-    content: "# Here we are at #{i + 1}\n" + post_content,
+    content: post_content,
     status: Post.statuses.keys.sample,
     user: admin,
-    categories: categories.sample(rand(1..4)),
-    tags: tags.sample(rand(1..5))
+    categories: categories.sample(rand(1..(categories.length - 1))),
+    tags: tags.sample(rand(1..(tags.length - 1)))
   )
 
   # Associate random categories and tags
