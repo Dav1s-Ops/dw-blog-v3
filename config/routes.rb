dbbc4b4d319a686bb6492a0408561e53c0cc330f
Davis Weimer <davisweimer@gmail.com>
2024-12-29 11:25:10 -0700

    Add active_storage route

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
   # Posts by category sort
   get 'posts/category/:category', to: 'posts#index', as: 'category_posts'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -30,6 +28,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  draw :active_storage
 end
