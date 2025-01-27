4b4dfb3b4eaf554679b91f2521f6f301181eedc4
Davis Weimer <davisweimer@gmail.com>
2025-01-26 17:10:18 -0700

    Configure ngrok stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.hosts << /.*\.ngrok\.app/
+  config.hosts << /.*\.ngrok\.io/
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
