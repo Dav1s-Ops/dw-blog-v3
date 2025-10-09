8de5170cb07dc718f77bec12840a3d41798d08d9
Davis Weimer <davisweimer@gmail.com>
2025-10-08 19:17:03 -0600

    Generate ContactMailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.feedback.subject
+  #
+  def feedback
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
