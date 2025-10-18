ab39acb2396299229c9dcee88f2df5b63800b2cc
Davis Weimer <davisweimer@gmail.com>
2025-10-18 14:59:28 -0600

    Try selenium another day
    I would rather use Playwright

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,10 +76,6 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers
 end
 
-Capybara.default_driver = :selenium_chrome_headless
-Capybara.javascript_driver = :selenium_chrome_headless
-Capybara.server = :puma
-
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
